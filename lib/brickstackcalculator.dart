f4b6b95cbe00f3df9e91d3c2f53a7b9b9a8f43a8
Colin Newton-Harris <col1695@hotmail.com>
2025-03-13 21:06:40 +0000

    added buttonstyle variable to define attributes for all buttons

---
+++ lib/brickstackcalculator.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Brickstackcalculator extends StatelessWidget {
+  const Brickstackcalculator({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Brick Stack Calculator"),
+      ),
+    );
+  }
+}
--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,3 +1,4 @@
+import 'package:brickieapp/brickstackcalculator.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:brickieapp/gablebrickcalculator.dart';
@@ -6,8 +7,20 @@ import 'package:brickieapp/gablebrickcalculator.dart';
 class Homepage extends StatelessWidget {
   const Homepage({super.key});
 
+
+
   @override
   Widget build(BuildContext context) {
+
+    var buttonStyle = ButtonStyle(
+      backgroundColor: WidgetStatePropertyAll<Color>(Colors.white),
+      foregroundColor: WidgetStatePropertyAll<Color>(Colors.black),
+      shadowColor: WidgetStatePropertyAll<Color>(Colors.black),
+      elevation: WidgetStatePropertyAll<double>(10),
+      maximumSize: WidgetStatePropertyAll<Size>(Size(300, 50)),
+      minimumSize: WidgetStatePropertyAll<Size>(Size(300, 50)),
+    );
+
     return Scaffold(
       appBar: AppBar(
         title: Text("Brickie Wiki"),
@@ -17,16 +30,16 @@ class Homepage extends StatelessWidget {
           Center(
           ),
           SizedBox(height: 20),
-          ElevatedButton(style: ButtonStyle(backgroundColor: WidgetStatePropertyAll<Color>(Colors.orange),),
+          ElevatedButton(style: buttonStyle,
             onPressed: () {
               Navigator.push(context, MaterialPageRoute(builder: (context) => Gablebrickcalculator()));
             },
             child: Text("Gable Brick Calculator"),
           ),
           SizedBox(height: 20),
-          ElevatedButton(style: ButtonStyle(backgroundColor: WidgetStatePropertyAll<Color>(Colors.blue)),
+          ElevatedButton(style: buttonStyle,
             onPressed: () {
-              Navigator.push(context, MaterialPageRoute(builder: (context) => Gablebrickcalculator()));
+              Navigator.push(context, MaterialPageRoute(builder: (context) => Brickstackcalculator( )));
             },
             child: Text("Brick Stack Calculator"),
           )
